c1025fde63630cf8d3b7fba2768c431ecde9d9c7
padidaapp <padidaapp@gmail.com>
2020-02-05 12:50:55 +0530

    final working

---
+++ app/src/main/java/com/example/padidav2/loginactivity.java
@@ -0,0 +1,16 @@
+package com.example.padidav2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginactivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginactivity);
+    }
+}
